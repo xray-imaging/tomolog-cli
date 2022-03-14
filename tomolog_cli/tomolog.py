bef264513574712f4aa482b071458d2838f357d4
Francesco De Carlo <decarlof@gmail.com>
2022-03-14 17:39:21 -0500

    corrected handling of recon binning

--- tomolog_cli/tomolog.py
+++ tomolog_cli/tomolog.py
@@ -25,10 +25,6 @@ DROPBOX_TOKEN   = os.path.join(str(pathlib.Path.home()), 'tokens', 'dropbox_toke
 GOOGLE_TOKEN    = os.path.join(str(pathlib.Path.home()), 'tokens', 'google_token.json')
 
 class TomoLog():
-    '''
-    Class to publish experiment meta data, tomography projection and reconstruction on a 
-    google slide document.
-    '''
     def __init__(self):
 
         self.snippets  = auth.google(GOOGLE_TOKEN)
@@ -83,6 +79,9 @@ class TomoLog():
         full_file_name = meta[self.full_file_name_key][0]
         self.snippets.create_textbox_with_text(presentation_id, page_id, os.path.basename(
             full_file_name)[:-3], 50, 400, 0, 0, 18)
+        dims          = meta[self.data_size_key][0].replace("(", "").replace(")", "").split(',')
+        width         = int(dims[2])
+        height        = int(dims[1])
 
         if meta[self.exposure_time_key][1] == None:
             log.warning('Exposure time units are missing assuming (s)')
@@ -114,14 +113,14 @@ class TomoLog():
         proj = reads.read_raw(args)
  
         if(args.beamline == '32-id'):
-            log.info('plotting nanoCT projection')
+            log.info('Plotting nanoCT projection')
             # 32-id datasets may include both nanoCT and microCT data as proj[0] and proj[1] respectively
             fname = FILE_NAME_PROJ0+'.jpg'
             nct_resolution = self.resolution / 1000.
             plots.plot_projection(proj[0], fname, resolution=nct_resolution)
             self.publish_projection(fname, presentation_id, page_id, 0, 100)
             try:
-                log.info('plotting microCT projection')
+                log.info('Plotting microCT projection')
                 fname = FILE_NAME_PROJ1+'.jpg'
                 mct_resolution = self.pixel_size / self.magnification
                 plots.plot_projection(proj[1], fname, resolution=mct_resolution)
@@ -129,7 +128,7 @@ class TomoLog():
             except:
                 log.warning('No microCT data available')
         else:
-            log.info('plotting microCT projection')
+            log.info('Plotting microCT projection')
             fname = FILE_NAME_PROJ0+'.jpg'
             plots.plot_projection(proj[0], fname, resolution=self.resolution)
             self.publish_projection(fname, presentation_id, page_id, 0, 100)
@@ -137,13 +136,13 @@ class TomoLog():
             presentation_id, page_id, 'Reconstruction', 30, 150, 270, 0, 10)
 
         # read reconstructions
-        recon = reads.read_recon(args, meta)    
+        recon, binning_rec = reads.read_recon(args, meta)    
         # publish reconstructions
         if len(recon) == 3:
             # prepare reconstruction
             if(args.beamline == '32-id'):
                 self.resolution = self.resolution / 1000.
-            self.resolution = self.resolution * self.binning
+            self.resolution = self.resolution * self.binning * binning_rec
             plots.plot_recon(args, dims, recon, FILE_NAME_RECON, self.resolution)
             with open(FILE_NAME_RECON, 'rb') as f:
                 self.dbx.files_upload(f.read(), '/'+FILE_NAME_RECON, dropbox.files.WriteMode.overwrite)
@@ -160,4 +159,9 @@ class TomoLog():
             proj_url = self.dbx.files_get_temporary_link('/'+fname).link
             self.snippets.create_image(presentation_id, page_id, proj_url, 210, 210, posx, posy)
 
+    def publish_recon(self, fname, presentation_id, page_id, posx, posy):
+        with open(fname, 'rb') as f:
+            self.dbx.files_upload(f.read(), '/'+fname, dropbox.files.WriteMode.overwrite)
+            proj_url = self.dbx.files_get_temporary_link('/'+fname).link
+            self.snippets.create_image(presentation_id, page_id, proj_url, 210, 210, posx, posy)
 
