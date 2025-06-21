8c68880d59b30844232f9be1efebd6c24da843af
aps-7bm <aps.7bm@gmail.com>
2025-06-20 19:19:53 -0500

    cleanup warning

--- src/tomolog_cli/__init__.py
+++ src/tomolog_cli/__init__.py
@@ -1,11 +1,3 @@
-from pkg_resources import get_distribution, DistributionNotFound
-
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    pass
-    
 from tomolog_cli.config import *
 from tomolog_cli.log import *
 from tomolog_cli.auth import *
